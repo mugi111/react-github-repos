30d770df3696728b036a85f38ef8ea6d42752d77
mugi <m.111_t.1230_nittc@outlook.com>
2020-09-13 06:21:09 +0900

    content download

--- src/Components/Language.tsx
+++ src/Components/Language.tsx
@@ -1,7 +1,19 @@
-import * as React from "react";
+import React, { useEffect, useState } from "react";
 import { LangProps } from "../Types";
+import { Octokit } from "@octokit/rest";
 
 const Language: React.FC<LangProps> = (props: LangProps) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const octokit = new Octokit();
+      const result = await octokit.repos.get({
+        owner: "github",
+        repo: "linguist",
+        path: "lib/linguist/languages.yml",
+      });
+    };
+    fetchData();
+  }, []);
   return (
     <span>
       <span>{props.name}</span>
