8ee348bea40fae367b7779ee4e93a54e7ff6e4df
mugi <m.111_t.1230_nittc@outlook.com>
2020-09-23 03:08:30 +0900

    add lang scss

--- src/Components/Language.tsx
+++ src/Components/Language.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { LangProps } from "../Types";
+import "../Styles/Language.scss";
 
 const Language: React.FC<LangProps> = (props: LangProps) => {
   return (
-    <span>
-      <span>{props.name}</span>
-    </span>
+    <p className="github-repos-language-p">
+      <span className="github-repos-language-span">
+        <span>{props.name}</span>
+      </span>
+    </p>
   );
 };
 
