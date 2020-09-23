45d5a22b63987a811216630a809c987518feb9c8
Takumi Hirunuma <m.111_t.1230_nittc@outlook.com>
2020-09-23 15:20:24 +0900

    update lang color class name

--- src/Components/Language.tsx
+++ src/Components/Language.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { LangProps } from "../Types";
+import colorList from "../Styles/colors.json";
 import "../Styles/Language.scss";
 
 const Language: React.FC<LangProps> = (props: LangProps) => {
   return (
     <span className="github-repos-language-span">
+      <span className="github-repos-langage-color" style={{backgroundColor: "red"}} />
       <span>{props.name}</span>
     </span>
   );
