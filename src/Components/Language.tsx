1a0b8a71591ddccd5c6d9bbc314404e65f5798c4
Takumi Hirunuma <m.111_t.1230_nittc@outlook.com>
2020-09-23 16:23:41 +0900

    get lang color

--- src/Components/Language.tsx
+++ src/Components/Language.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import { LangProps } from "../Types";
-import colorList from "../Styles/colors.json";
 import "../Styles/Language.scss";
 
+const colorList = require("../Styles/colors.json");
+
+
 const Language: React.FC<LangProps> = (props: LangProps) => {
-  return (
-    <span className="github-repos-language-span">
-      <span className="github-repos-langage-color" style={{backgroundColor: "red"}} />
-      <span>{props.name}</span>
-    </span>
-  );
+  if(props.name === undefined) {
+    return (
+      <span className="github-repos-language-span" style={{display: "none"}}>
+        <span className="github-repos-langage-color" />
+        <span>{props.name}</span>
+      </span>
+    );
+  } else {
+    return (
+      <span className="github-repos-language-span">
+        <span className="github-repos-langage-color" style={{backgroundColor: colorList[props.name.toLowerCase()].color}} />
+        <span>{props.name}</span>
+      </span> 
+    )
+  }
+
 };
 
 export default Language;
