85815941dd3960c67c4ac81c23b0b464e72ccc5e
mugi <m.111_t.1230_nittc@outlook.com>
2020-09-27 04:42:14 +0900

    site about install footer

--- site/src/App.js
+++ site/src/App.js
@@ -11,8 +11,29 @@ const props = [
 
 function App() {
   return (
-    <div>
-      <GithubRepos repoProps={props} />
+    <div className="content-page content-background">
+      <header>
+        <h1>React Github Repos</h1>
+      </header>
+      <div className="content about">
+        <h2>About</h2>
+        <span>Githubのpinned欄みたいにリポジトリを表示するコンポーネント</span>
+      </div>
+      <div className="content about">
+        <h2>Usage</h2>
+        <h3>Install</h3>
+        <span className="code-bg">yarn add react-github-repos</span>
+        <span className="code-bg">npm i react-github-repos</span>
+        <h3></h3>
+      </div>
+      <h3></h3>
+      <div className="content example">
+        <h2>Example</h2>
+        <div>
+          <GithubRepos repoProps={props} />
+        </div>
+      </div>
+      <footer className="footer">© 2020 mugi111</footer>
     </div>
   );
 }
