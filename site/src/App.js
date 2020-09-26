9009a76a9ec2197c0047b36c5d499796276724ee
mugi <m.111_t.1230_nittc@outlook.com>
2020-09-27 05:55:38 +0900

    add sample code

--- site/src/App.js
+++ site/src/App.js
@@ -22,11 +22,34 @@ function App() {
       <div className="content about">
         <h2>Usage</h2>
         <h3>Install</h3>
-        <span className="code-bg">yarn add react-github-repos</span>
-        <span className="code-bg">npm i react-github-repos</span>
-        <h3></h3>
+        <code className="code-bg">yarn add react-github-repos</code>
+        <code className="code-bg">npm i react-github-repos</code>
+        <h3>Code</h3>
+        <pre>
+          <code className="code-bg">
+            {`import React from "react";
+import GithubRepos from "react-github-repos";
+import "./App.css";
+import "react-github-repos/dist/index.css";
+
+const props = [
+  { owner: "mugi111", repo: "mugi111" },
+  { owner: "mugi111", repo: "react-github-repos" },
+  { owner: "mugi111", repo: "tweet-trigger-release" },
+];
+
+function App() {
+  return (
+    <div>
+      <GithubRepos repoProps={props} />
+    </div>
+  );
+}
+
+export default App;`}
+          </code>
+        </pre>
       </div>
-      <h3></h3>
       <div className="content example">
         <h2>Example</h2>
         <div>
